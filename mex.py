4e89d9f5dbb78e6fa7c5f3c1f06601438df2c4bc
Daniel <dlu0@sunrise.ch>
2024-02-17 04:34:55 +0100

    Add files via upload
    V1.3
    - Genauere Debug-Informationen
    - Code Kosmetik

--- mex.py
+++ mex.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 ##################################
-#             V1.2               #
+#             V1.3               #
 # MEX-Daten in ioBroker einlesen #
 #    Benötigt den MQTT Adapter   #
 #    (C) 2024 Daniel Luginbühl   #
@@ -45,11 +45,11 @@ def login():
     if debug:
         print('Login in...')
     global session_id
-    global logged_in
     url = "https://api.heizoel24.de/app/api/app/Login"
     newHeaders = {'Content-type': 'application/json'}
     reply = requests.post(url, json = { "Password" : passwort, "Username" : username}, headers=newHeaders)
 
+    return_flag = False
     if reply.status_code == 200:
         if debug:
             print("Login OK")
@@ -58,12 +58,19 @@ def login():
             session_id = reply_json['SessionId']
             if debug:
                 print('Session ID: ' + session_id)
-            logged_in = True
+            return_flag = True
+        else:
+            if debug:
+                print('ResultCode nicht 0. Keine Session ID erhalten!')
     else:
-        print('Login nicht OK! Heizoel24 Login Status Code: ' + str(reply.status_code))
+        if debug:
+            print('Login fehlgeschlagen! Heizoel24 Login Status Code: ' + str(reply.status_code))
+    return return_flag
 
 def mex():  
-    login()
+    login_status = login()
+    if login_status == False:
+        return "error"
     if debug:
         print('Refresh sensor data cache...')
     url = 'https://api.heizoel24.de/app/api/app/GetDashboardData/'+ session_id + '/1/1/False'
@@ -79,21 +86,24 @@ def mex():
     return sensor_data
 
 def main():
-
     topic1 = ['SensorId', 'IsMain', 'CurrentVolumePercentage', 'CurrentVolume', 'NotifyAtLowLevel', 'NotifyAtAlmostEmptyLevel', 'NotificationsEnabled', 'Usage', 'RemainsUntil', 'MaxVolume', 'ZipCode', 'MexName', 'LastMeasurementTimeStamp', 'LastMeasurementWithDifferentValue', 'BatteryPercentage', 'Battery', 'LitresPerCentimeter', 'LastMeasurementWasSuccessfully', 'SensorTypeId', 'HasMeasurements', 'MeasuredDaysCount', 'LastMeasurementWasTooHigh', 'YearlyOilUsage', 'RemainingDays', 'LastOrderPrice', 'ResultCode', 'ResultMessage']
-
     topic2 = ['LastOrderPrice', 'PriceComparedToYesterdayPercentage', 'PriceForecastPercentage', 'HasMultipleMexDevices', 'DashboardViewMode', 'ShowComparedToYesterday', 'ShowForecast', 'ResultCode', 'ResultMessage']
-
     RemainsUntilCombined = ['MonthAndYear', 'RemainsValue', 'RemainsUnit']
 
     try:
-        client = mqtt.Client("MEX")
+        client = mqtt.Client("mex")
         if debug:
             print("paho-mqtt version < 2.0")
     except:
-        client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "MEX")
+        client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "mex")
         if debug:
             print("paho-mqtt version >= 2.0")
+
+    if debug:
+        print("---------------------")
+        print("client:", client)
+        print("---------------------")
+
     client.username_pw_set(mqtt_user, mqtt_pass)
     client.connect(broker_address)
 
@@ -108,7 +118,11 @@ def main():
     daten = daten.json()
 
     if debug:
+        print("JSON-Daten:")
+        print("===========")
+        print()
         print(daten)
+        print()
 
     if debug:
         print("---------------------")
@@ -126,7 +140,7 @@ def main():
         if debug:
             print(topic1[n] + ":", daten[topic1[n]])
         mqtt_send(client, "Items/" + topic1[n], daten[topic1[n]])
-    mqtt_send(client, "Items/DataReceived", True)    
+    mqtt_send(client, "Items/DataReceived", True)
 
     daten3 = daten['RemainsUntilCombined']
 
@@ -140,7 +154,7 @@ def main():
 
     client.disconnect()
 
-#### Beispiel Ausgaben. Diese können dann natürlich auch in Datenpunkte geschrieben werden. ####
+#### Diverse Beispiele ####
 #    print()
 #    print("SensorId:                 ", daten["SensorId"])
 #    print("Aktueller Inhalt in %:    ", daten["CurrentVolumePercentage"])
