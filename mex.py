27c597e4e295dc47a15f6018ca27decd7788cd93
Daniel <dlu0@sunrise.ch>
2024-03-19 23:57:10 +0100

    Add files via upload
    V2.3
    - Code gesäubert (pylint)

--- mex.py
+++ mex.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 ###################################################################################################
-#################################             V2.2               ##################################
+#################################             V2.3               ##################################
 #################################  MEX-Daten per MQTT versenden  ##################################
 #################################   (C) 2024 Daniel Luginbühl    ##################################
 ###################################################################################################
@@ -23,72 +23,74 @@
 ################    pip3 install typing-extensions                                #################
 ###################################################################################################
 
-###################################################################################################
-################################### Hier Einträge anpassen! #######################################
-
-username = "aaaaa@gmail.com"    # Deine Email Adresse bei HeizOel24
-passwort = "BBBBBBBBB"          # Dein Passwort bei HeizOel24
-
-broker_address = "192.168.1.50" # MQTT Broker IP (da wo der MQTT Broker läuft)
-mqtt_user = "uuuuuu"            # MQTT User      (im MQTT Broker definiert)
-mqtt_pass = "pppppp"            # MQTT Passwort  (im MQTT Broker definiert)
-mqtt_port = 1883                # MQTT Port      (default: 1883)
-mex_id = 1                      # Falls mehrere MEX Geräte, hier ID eintragen (2, 3, 4, ...)
-wenigerDaten = True             # Weniger Zukunftsdaten abrufen (nur alle 14 Tage)
-erstelleJson = True             # True, wenn CalculatedRemaining.json erstellt werden soll
-jsonPfad = ""                   # Pfad für die Json Datei. Standardpfad ist bei Script.
-#                                 sonst zBsp.: jsonPfad = "/home/pi/"
-
-delay = False                   # Auf True setzen, wenn der MQTT Broker nur die 1. Zeile empfängt
-debug = False                   # True = Debug Infos auf die Konsole.
-#                                 Unbedingt nach debuggen zurück setzen!!
-#                                 Be sure to reset after debugging!!
-###################################################################################################
-###################################################################################################
-
-
+""" Importe. Hier nichts verändern!"""
 import time
 import json
 import random
 import requests
 import paho.mqtt.client as mqtt
 
+###################################################################################################
+################################### Hier Einträge anpassen! #######################################
+
+USERNAME = "uuuuu@gmail.com"    # Deine Email Adresse bei HeizOel24
+PASSWORD = "ppppppppp"          # Dein Passwort bei HeizOel24
+MEX_ID = 1                      # Falls mehrere MEX Geräte, hier ID eintragen (2, 3, 4, ...)
+
+BROKER_ADDRESS = "192.168.1.50" # MQTT Broker IP (da wo der MQTT Broker läuft)
+MQTT_USER = "xxxxxx"            # MQTT User      (im MQTT Broker definiert)
+MQTT_PASS = "yyyyyy"            # MQTT Passwort  (im MQTT Broker definiert)
+MQTT_PORT = 1883                # MQTT Port      (default: 1883)
+
+LESS_DATA = True                # Weniger Zukunftsdaten abrufen (nur alle 14 Tage)
+CREATE_JSON = True              # True, wenn CalculatedRemaining.json erstellt werden soll
+JSON_PATH = ""                  # Pfad für die Json Datei. Standardpfad ist bei Script.
+                                # sonst zBsp.: jsonPfad = "/home/pi/"
+
+DELAY = False                   # Auf True setzen, wenn der MQTT Broker nur die 1. Zeile empfängt
+DEBUG = False                   # True = Debug Infos auf die Konsole.
+                                # Unbedingt nach debuggen zurück setzen!!
+                                # Be sure to reset after debugging!!
+
+###################################################################################################
+###################################################################################################
+
 # Zufällige Zeitverzögerung 0 bis 3540 Sekunden (0-59min)
 verzoegerung = random.randint(0,3540)
-if debug:
+if DEBUG:
     verzoegerung = random.randint(0,5)
 print("Datenabfrage startet in", verzoegerung, "Sekunden")
 time.sleep(verzoegerung)
 
-def SendMqtt(client, topic, wert, mex_id):
+def send_mqtt(client, topic, wert):
     """Send MQTT"""
-    client.publish("MEX/" + str(mex_id) + "/" + topic, wert, 0, True)
+    client.publish("MEX/" + str(MEX_ID) + "/" + topic, wert, 0, True)
 
 def login():
     """Login to Heizoel24 server"""
-    if debug:
+    if DEBUG:
         print("Login in...")
     url = "https://api.heizoel24.de/app/api/app/Login"
-    newHeaders = {"Content-type": "application/json"}
+    new_headers = {"Content-type": "application/json"}
     reply = requests.post(
-        url, json = { "Password" : passwort, "Username" : username}, headers=newHeaders, timeout=5
+        url, json = { "Password" : PASSWORD, "Username" : USERNAME}, headers=new_headers, timeout=5
     )
 
     return_flag = False
     if reply.status_code == 200:
-        if debug:
+        if DEBUG:
             print("Login OK")
         reply_json = json.loads(reply.text)
         if reply_json["ResultCode"] == 0:
             session_id = reply_json["SessionId"]
-            if debug:
+            if DEBUG:
                 print("Session ID: " + session_id)
             return_flag = True
         else:
-            if debug:
+            if DEBUG:
                 print("ResultCode nicht 0. Keine Session ID erhalten!")
     else:
-        if debug:
+        if DEBUG:
             print("Login fehlgeschlagen! Heizoel24 Login Status Code: " + str(reply.status_code))
     return return_flag, session_id
 
@@ -97,87 +99,93 @@ def mex():
     login_status, session_id = login()
     if not login_status:
         return "error"
-    if debug:
+    if DEBUG:
         print("Refresh sensor data cache...")
-    url = "https://api.heizoel24.de/app/api/app/GetDashboardData/"+ session_id + "/1/" + str(mex_id) + "/False"
+    url = "https://api.heizoel24.de/app/api/app/GetDashboardData/"\
+          + session_id + "/1/" + str(MEX_ID) + "/False"
     reply = requests.get(url, timeout=5)
     if reply.status_code == 200:
-        if debug:
-            print("MEX ID:", mex_id)
+        if DEBUG:
+            print("MEX ID:", MEX_ID)
             print("Daten wurden empfangen")
         sensor_data = reply
     else:
-        if debug:
+        if DEBUG:
             print("Heizoel24 GetDashboardData > Status Code: " + str(reply.status_code))
         sensor_data = "error"   # Fehler. Keine Daten empfangen.
     return sensor_data, session_id
 
 def measurement(sensor_id, session_id):
     """Berechnete zukünftige Oelstände holen"""
-    if debug:
+    if DEBUG:
         print("Hole zukünftige Oelstände...")
-    url = "https://api.heizoel24.de/app/api/app/measurement/CalculateRemaining/"+ session_id + "/" + str(sensor_id) + "/False"
+    url = "https://api.heizoel24.de/app/api/app/measurement/CalculateRemaining/"\
+          + session_id + "/" + str(sensor_id) + "/False"
     reply = requests.get(url, timeout=5)
     if reply.status_code == 200:
-        if debug:
+        if DEBUG:
             print("Berechnete zukünftige Oelstände wurden empfangen")
         zukunfts_daten = reply
     else:
-        if debug:
+        if DEBUG:
             print("Heizoel24 Oelstände > Status Code: " + str(reply.status_code))
         zukunfts_daten = "error"   # Fehler. Keine Daten empfangen.
     return zukunfts_daten
 
 def main():
     """Hauptroutine"""
-    Items = [
+    items = [
         "SensorId", "IsMain", "CurrentVolumePercentage", "CurrentVolume", "NotifyAtLowLevel",
-        "NotifyAtAlmostEmptyLevel", "NotificationsEnabled", "Usage", "RemainsUntil", "MaxVolume",
-        "ZipCode", "MexName", "LastMeasurementTimeStamp", "LastMeasurementWithDifferentValue", "BatteryPercentage",
-        "Battery", "LitresPerCentimeter", "LastMeasurementWasSuccessfully", "SensorTypeId", "HasMeasurements",
-        "MeasuredDaysCount", "LastMeasurementWasTooHigh", "YearlyOilUsage", "RemainingDays", "LastOrderPrice",
+        "NotifyAtAlmostEmptyLevel", "NotificationsEnabled", "Usage", "RemainsUntil",
+        "MaxVolume", "ZipCode", "MexName", "LastMeasurementTimeStamp",
+        "LastMeasurementWithDifferentValue", "BatteryPercentage", "Battery", "LitresPerCentimeter",
+        "LastMeasurementWasSuccessfully", "SensorTypeId", "HasMeasurements", "MeasuredDaysCount",
+        "LastMeasurementWasTooHigh", "YearlyOilUsage", "RemainingDays", "LastOrderPrice",
         "ResultCode", "ResultMessage"
     ]
-    PricingForecast = [
-        "LastOrderPrice", "PriceComparedToYesterdayPercentage", "PriceForecastPercentage",
-        "HasMultipleMexDevices", "DashboardViewMode", "ShowComparedToYesterday",
-        "ShowForecast", "ResultCode", "ResultMessage"
+    pricing_forecast = [
+        "LastOrderPrice",        "PriceComparedToYesterdayPercentage", "PriceForecastPercentage",
+        "HasMultipleMexDevices", "DashboardViewMode",                  "ShowComparedToYesterday",
+        "ShowForecast",          "ResultCode",                         "ResultMessage"
     ]
-    RemainsUntilCombined = ["MonthAndYear", "RemainsValue", "RemainsUnit"]
+    remains_until_combined = [
+        "MonthAndYear", "RemainsValue", "RemainsUnit"
+    ]
 
+    if DEBUG:
+        print("------------------------")
     try:
         client = mqtt.Client("mex")
-        if debug:
+        if DEBUG:
             print("paho-mqtt version < 2.0")
-    except:
+    except ValueError:
         client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "mex")
-        if debug:
+        if DEBUG:
             print("paho-mqtt version >= 2.0")
 
-    if debug:
-        print("---------------------")
-        print("client:", client)
-        print("---------------------")
+    if DEBUG:
+        print("------------------------")
 
-    client.username_pw_set(mqtt_user, mqtt_pass)
+    client.username_pw_set(MQTT_USER, MQTT_PASS)
     try:
-        client.connect(broker_address, port=mqtt_port)
-    except:
+        client.connect(BROKER_ADDRESS, port=MQTT_PORT)
+    except OSError as error:
         print("Verbindung zum MQTT-Broker fehlgeschlagen")
         print("Connection to MQTT broker failed")
+        print(error)
         exit(1)
 
     daten, session_id = mex()
     if daten == "error":
-        if debug:
+        if DEBUG:
             print("Fehler. Keine Daten empfangen.")
-        SendMqtt(client, "Items/DataReceived", False, mex_id)
+        send_mqtt(client, "Items/DataReceived", False)
         client.disconnect()
         return
 
     daten = daten.json()
 
-    if debug:
+    if DEBUG:
         print()
         print("JSON-Daten:")
         print("===========")
@@ -186,59 +194,59 @@ def main():
         print()
         print("---------------------")
         print()
-    for n in range(len(PricingForecast)):
-        if debug:
-            print(PricingForecast[n] + ":", daten[PricingForecast[n]])
-        if (daten[PricingForecast[n]] == False) and (
-            PricingForecast[n] == "PriceComparedToYesterdayPercentage" or
-            PricingForecast[n] == "PriceForecastPercentage"):
-            if debug:
-                print(PricingForecast[n], "übersprungen weil 'False'")
+    for n, pricing_forecast in enumerate(pricing_forecast):
+        if DEBUG:
+            print(pricing_forecast + ":", daten[pricing_forecast])
+        if (daten[pricing_forecast] is False) and (
+            pricing_forecast == "PriceComparedToYesterdayPercentage" or
+            pricing_forecast == "PriceForecastPercentage"):
+            if DEBUG:
+                print(pricing_forecast, "übersprungen weil 'False'")
         else:
-            SendMqtt(client, "PricingForecast/" + PricingForecast[n], daten[PricingForecast[n]], mex_id)
-        if delay:
+            send_mqtt(client, "PricingForecast/" + pricing_forecast, daten[pricing_forecast])
+        if DELAY:
             time.sleep(0.05)
 
     daten = daten["Items"]
     daten = daten[0]
 
-    if debug:
+    if DEBUG:
         print("---------------------")
-    for n in range(len(Items)):
-        if debug:
-            print(Items[n] + ":", daten[Items[n]])
-        SendMqtt(client, "Items/" + Items[n], daten[Items[n]], mex_id)
-        if delay:
+    for n, items in enumerate(items):
+        if DEBUG:
+            print(items + ":", daten[items])
+        send_mqtt(client, "Items/" + items, daten[items])
+        if DELAY:
             time.sleep(0.05)
-    SendMqtt(client, "Items/DataReceived", True, mex_id)
+    send_mqtt(client, "Items/DataReceived", True)
 
     sensor_id = daten["SensorId"]
-    print("******* sensor, session:", sensor_id, session_id)
 
     daten3 = daten["RemainsUntilCombined"]
 
-    if debug:
+    if DEBUG:
         print("---------------------")
         print("RemainsUntilCombined:")
-    for n in range(len(RemainsUntilCombined)):
-        if debug:
-            print(RemainsUntilCombined[n] + ":", daten3[RemainsUntilCombined[n]])
-        SendMqtt(client, "RemainsUntilCombined/" + RemainsUntilCombined[n], daten3[RemainsUntilCombined[n]], mex_id)
-        if delay:
+    for n, remains_until_combined in enumerate(remains_until_combined):
+        if DEBUG:
+            print(remains_until_combined + ":", daten3[remains_until_combined])
+        send_mqtt(client, "RemainsUntilCombined/" + remains_until_combined,
+                 daten3[remains_until_combined])
+        if DELAY:
             time.sleep(0.05)
 
     zukunfts_daten = measurement(sensor_id, session_id)
 
     if zukunfts_daten == "error":
-        if debug:
+        if DEBUG:
             print("Fehler. Keine Daten empfangen.")
             return
 
     zukunfts_daten = zukunfts_daten.json()
 
-    if erstelleJson:
+    if CREATE_JSON:
         json_object = json.dumps(zukunfts_daten, indent=4)
-        with open(jsonPfad + "CalculatedRemaining.json","w") as datei:
+        with open(JSON_PATH + "CalculatedRemaining.json","w", encoding="utf-8") as datei:
             datei.write(json_object)
 
     zukunfts_daten = zukunfts_daten["ConsumptionCurveResult"]
@@ -246,14 +254,16 @@ def main():
     n = 0
     for key in zukunfts_daten:
         ausfuehren = True
-        if wenigerDaten:
+        if LESS_DATA:
             ausfuehren = n % 14 == 0
         if ausfuehren:
-            if debug:
+            if DEBUG:
                 print(key.split("T")[0], zukunfts_daten[key], "Liter remaining")
-            SendMqtt(client, "CalculatedRemaining/Today_plus_" + str(n).zfill(4) + "_Days", str(key).split("T")[0] + " = " + str(zukunfts_daten[key]) + " Ltr.", mex_id)
+            send_mqtt(client, "CalculatedRemaining/Today_plus_" + str(n).zfill(4) +
+                      "_Days", str(key).split("T", maxsplit=1)[0]\
+                      + " = " + str(zukunfts_daten[key]) + " Ltr.")
         n+=1
-        if delay:
+        if DELAY:
             time.sleep(0.05)
     client.disconnect()
 
